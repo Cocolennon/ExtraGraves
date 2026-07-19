0e735d8d871ec64702581d34a28fe0e9483dcf3b
Cocolennon <corentin71.lopez@gmail.com>
2026-07-20 01:09:39 +0200

    Made breaking graves outside of menus "safer"

--- src/main/java/me/cocolennon/extragraves/listeners/BlockBreakListener.java
+++ src/main/java/me/cocolennon/extragraves/listeners/BlockBreakListener.java
@@ -1,8 +1,12 @@
 package me.cocolennon.extragraves.listeners;
 
+import com.nexomc.nexo.api.NexoBlocks;
+import com.nexomc.nexo.utils.drops.Drop;
+import me.cocolennon.extragraves.Main;
 import me.cocolennon.extragraves.util.GraveHelper;
 import me.cocolennon.extragraves.util.Helper;
 import me.cocolennon.extragraves.util.Localization;
+import org.bukkit.Material;
 import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
@@ -15,6 +19,8 @@ public class BlockBreakListener implements Listener {
         Block block = event.getBlock();
         if(!Helper.isGrave(block)) return;
         event.setDropItems(false);
+        if(Main.getInstance().config().graveBlockName.startsWith("nexo-")) NexoBlocks.remove(block.getLocation(), null, Drop.emptyDrop());
+        else block.setType(Material.AIR);
         GraveHelper.dropGrave(block);
         Player player = event.getPlayer();
         player.sendMessage(Localization.get(player, "grave-destroyed", true));
